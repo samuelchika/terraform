5d911d3c365161431380f707ad8c1e9e7fcbb784
samuelchika <samuelemyrs@gmail.com>
2021-04-15 16:47:26 +0100

    ec2 instance with parameters

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
 provider "aws" {
   region = "eu-west-1"
-  access_key = "ur access key"
-  secret_key = "your secret key"
+  # access_key = "ur access key"
+  # secret_key = "your secret key"
 }
 
-##automate the creating of keypair
+// TODO: automate the creating of keypair
+
+# variables
+variable "vpc_id" {
+  description = "vpc default id"
+  type = string
+}
+variable "ssh_ip" {
+  description = "My ip address for ssh ingress in the security group"
+  type = list(string)
+}
+variable "default_cidr_block" {
+  description = "Default cidr_block for ingress and egress 0.0.0.0/0"
+  type = list(string)
+}
 
 #output
 output "server_id" {
@@ -19,26 +33,46 @@ output "server_public_dns" {
     description = "Globa IP address"
     value = aws_instance.web_server.public_dns
 }
+// automating parameters
+#automate the default vpc
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+# GET THE DEFAULT INSTANCE.
+data "aws_ami" "amazon_linux_image" {
+  most_recent = true
+  owners = ["amazon"]
+  # this filters the images/ami and look for the one with the name starting with amxn2-ami-hvm- and has anything inbetween and ends with x86_64-gp2
+  filter {
+      name = "name"
+      values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  # we can filter with the virtualization-type
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 resource "aws_security_group" "web_server_sg" {
-    vpc_id = "default vpc"
+    vpc_id = data.aws_vpc.default_vpc.id
     ingress  {
         from_port = 22
-        to_port = 22
+        to_port = 22 
         protocol = "tcp"
-        cidr_blocks = ["ur ip address"]
+        cidr_blocks = var.ssh_ip
     }
     ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.default_cidr_block
     }
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.default_cidr_block
         prefix_list_ids = []
     }
     tags = {
@@ -47,21 +81,7 @@ resource "aws_security_group" "web_server_sg" {
 }
 
 #create ec2 instance
-# GET THE DEFAULT INSTANCE.
-data "aws_ami" "amazon_linux_image" {
-  most_recent = true
-  owners = ["amazon"]
-  # this filters the images/ami and look for the one with the name starting with amxn2-ami-hvm- and has anything inbetween and ends with x86_64-gp2
-  filter {
-      name = "name"
-      values = [ "amzn2-ami-hvm-*-x86_64-gp2" ]
-  }
-  # we can filter with the virtualization-type
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+
 resource "aws_instance" "web_server" {
   ami = data.aws_ami.amazon_linux_image.id
   instance_type = "t2.micro"
@@ -75,8 +95,7 @@ resource "aws_instance" "web_server" {
                 sudo systemctl start docker
                 sudo usermod -aG docker ec2-user
                 docker run -p 8080:80 nginx
-                EOF
-
+              EOF
 
   tags = {
       Name = "terrform-server"
